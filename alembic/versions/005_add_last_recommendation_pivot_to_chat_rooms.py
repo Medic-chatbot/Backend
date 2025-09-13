20fbd44808c17739bf71764f7d8895005d2182b7
yoonju977 <yoonju977@gmail.com>
2025-09-13 16:24:46 +0900

    Chore: allow Alembic version files to be tracked; add migration for chat window pivot

---
+++ alembic/versions/005_add_last_recommendation_pivot_to_chat_rooms.py
@@ -0,0 +1,29 @@
+"""
+Add last_recommendation_message_id to chat_rooms for windowed context
+
+Revision ID: 005_add_chatroom_pivot
+Revises: 004_create_disease_equipment_categories
+Create Date: 2025-09-13
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '005_add_chatroom_pivot'
+down_revision = '004_create_disease_equipment_categories'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        'chat_rooms',
+        sa.Column('last_recommendation_message_id', sa.Integer(), nullable=True),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column('chat_rooms', 'last_recommendation_message_id')
+
